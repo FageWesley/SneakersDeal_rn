a3869671a8f971615bca4b36c8632bda9469d955
Wesley <wesley.fage@ecole-hexagone.com>
2024-02-27 18:19:14 +0100

    modal ok need to style it !

--- screens/Filter.js
+++ screens/Filter.js
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View,Image, FlatList } from 'react-native'
-import React from 'react'
-import Ionsicons from 'react-native-vector-icons/Ionicons'
-import ProductCard from '../components/ProductCard'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  TextInput,
+} from "react-native";
+import React from "react";
+import Ionsicons from "react-native-vector-icons/Ionicons";
+import ProductCard from "../components/ProductCard";
+import { useState } from "react";
+import getAllProducts from "../FirebaseConfig";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Filter() {
-
   const [search, setSearch] = useState("");
   const allProducts = getAllProducts();
   const [filteredSearch, SetFilteredSearch] = useState([]);
@@ -13,86 +22,82 @@ export default function Filter() {
       allProducts.filter((products) => products.title.includes(searchTerm))
     );
   }
+  const navigation = useNavigation();
   return (
     <View style={styles.modal} animationType="slide">
-        <View style={styles.modal}>
-          <View style={styles.top}>
-            <Image
-              source={require("../assets/images/Logo.png")}
-              style={styles.logo}
-            ></Image>
-            <Text style={styles.title}>SneakersDeal</Text>
-            <Ionsicons
-              name={"close"}
-              color={"white"}
-              size={32}
-              style={styles.searchIcon}
-              onPress={() => navigation.goBack()}
-            />
-          </View>
-          <TextInput
-            placeholder="Search"
-            style={styles.searchInput}
-            onChangeText={(search) => {
-              setSearch(search);
-              handleFilter(search);
-            }}
-            ref={(input) => {
-              this.TextInput = input;
-            }}
-          ></TextInput>
+      <View style={styles.modal}>
+        <View style={styles.top}>
           <Ionsicons
             name={"close"}
-            color={"black"}
+            color={"white"}
             size={32}
-            style={styles.searchClearIcon}
-            onPress={() => {
-              setSearch("");
-              this.TextInput.clear();
-            }}
-            onScroll={() => Keyboard.dismiss()}
-          />
-          <FlatList
-            data={search == "" ? allProducts : filteredSearch}
-            renderItem={({ item }) => (
-              <ProductCard route="ProductPage" product={item}>
-                <Image source={{ uri: item.image }} style={styles.image} />
-              </ProductCard>
-            )}
-            keyExtractor={(item) => item.id}
-            numColumns={2}
-            style={styles.container}
-            contentContainerStyle={{ paddingBottom: 200 }}
+            style={styles.searchIcon}
+            onPress={() => navigation.goBack()}
           />
         </View>
+        <TextInput
+          placeholder="Search"
+          style={styles.searchInput}
+          onChangeText={(search) => {
+            setSearch(search);
+            handleFilter(search);
+          }}
+          ref={(input) => {
+            this.TextInput = input;
+          }}
+        ></TextInput>
+        <Ionsicons
+          name={"close"}
+          color={"black"}
+          size={32}
+          style={styles.searchClearIcon}
+          onPress={() => {
+            setSearch("");
+            this.TextInput.clear();
+          }}
+          onScroll={() => Keyboard.dismiss()}
+        />
+        <FlatList
+          data={search == "" ? allProducts : filteredSearch}
+          renderItem={({ item }) => (
+            <ProductCard route="ProductPage" product={item}>
+              <Image source={{ uri: item.image }} style={styles.image} />
+            </ProductCard>
+          )}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          style={styles.container}
+          contentContainerStyle={{ paddingBottom: 200 }}
+        />
       </View>
-  )
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    searchInput: {
-        width: 361,
-        height: 50,
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 5,
-        marginTop: 10,
-        alignSelf: "center",
-        paddingLeft: 5,
-      },
-      searchClearIcon: {
-        position: "absolute",
-        top: 108,
-        right: 20,
-      },
-      image: {
-        flexDirection: "row",
-        height: 150,
-        width: 150,
-        borderRadius: 0,
-        alignSelf: "flex-start",
-      },
-      container: {
-        alignSelf: "center",
-      },
-})
+  searchInput: {
+    width: 361,
+    height: 50,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 10,
+    alignSelf: "center",
+    paddingLeft: 5,
+  },
+  searchClearIcon: {
+    position: "absolute",
+    top: 108,
+    right: 20,
+  },
+  image: {
+    flexDirection: "row",
+    height: 150,
+    width: 150,
+    borderRadius: 0,
+    alignSelf: "flex-start",
+  },
+  container: {
+    alignSelf: "center",
+  },
+});
