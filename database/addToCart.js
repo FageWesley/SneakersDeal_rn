fb5c94137e8c6cc1d5d269a05847b55603686237
Wesley <wesley.fage@ecole-hexagone.com>
2024-02-28 22:29:08 +0100

    cart function to add seems ok and can update the quantity

--- database/addToCart.js
+++ database/addToCart.js
@@ -1,14 +1,42 @@
-import { ref, set } from "firebase/database";
+import { ref, set, update } from "firebase/database";
 import { FIREBASE_DATABASE } from "../FirebaseConfig";
 import getUserCart from "./getUserCart";
+import { isEqual } from "lodash";
+import { useState } from "react";
 
 
+export default function addToCart({ product }, quantity, userId) {
+  const cartId = userId + "cart";
+  const cart = getUserCart(cartId);
+  let isInArray = false;
+  let productToUpdate;
+  let currentQuantity;
 
-export default function addToCart({product},quantity,userId) {
-  const cartId = userId+"cart";
-  const cart = getUserCart(userId);
-  cart.push([product,quantity]);
-  const productToAdd = ref(FIREBASE_DATABASE, "cart/" + cartId+ "/");
+  // Verify if the product is already in cart
+  for (let i = 0; i < cart.length; i++) {
+    if (isEqual(cart[i][0], product)) {
+      isInArray = true;
+      productToUpdate = i;
+      currentQuantity = cart[i][1];
+      console.log(i);
+      console.log(productToUpdate);
+      break;
+    }
+  }
+  if (isInArray) {
+    console.log("Product already in cart, updating quantity");
+    updatedQuantity = currentQuantity + quantity;
+    const productToAdd = ref(
+      FIREBASE_DATABASE,
+      "cart/" + cartId + "/" + productToUpdate + "/" + 1 + "/"
+    );
+    set(productToAdd, updatedQuantity);
+  } else {
+    console.log("Product not in cart, adding to cart");
+    cart.push([product, quantity]);
+    const productToAdd = ref(FIREBASE_DATABASE, "cart/" + cartId);
     set(productToAdd, cart);
+  }
+
+  // console.log(JSON.stringify(cart));
 }
-  
--- database/getUserCart.js
+++ database/getUserCart.js
@@ -1,19 +1,15 @@
 import { ref, onValue } from "firebase/database";
 import { FIREBASE_DATABASE } from "../FirebaseConfig";
 
-export default function getUserCart(userId) {
-  const cartId = userId + "cart";
+export default function getUserCart(cartId) {
   const cartRef = ref(FIREBASE_DATABASE, "cart/" + cartId + "/");
-  const cartArray = [""];
+  const cartArray = [];
   onValue(cartRef, (snapshot) => {
-    try {
       const data = snapshot.val();
+      // console.log(data);
       for (let key in data) {
         cartArray.push(data[key]);
       }
-    } catch (error) {
-      return cartArray;
-    }
   });
 
   return cartArray;
